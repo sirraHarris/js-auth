fbcfc3074b3429047f7fccc4736de5f3666735b1
sirraHarris <harris337inc@gmail.com>
2023-09-24 11:20:39 +0300

    js-auth

--- src/route/auth.js
+++ src/route/auth.js
@@ -3,34 +3,71 @@ const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
 
-const Test = require('../class/test')
+const { User } = require('../class/user')
 
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/signup', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  return res.render('signup', {
     // вказуємо назву контейнера
-    name: 'index',
+    name: 'signup',
     // вказуємо назву компонентів
-    component: ['heading'],
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
 
     // вказуємо назву сторінки
-    title: 'Назва сторінки',
+    title: 'Signup page',
     // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
 
     // вказуємо дані,
     data: {
-      test: new Test().test,
+      role: [
+        { value: User.USER_ROLE.USER, text: 'Користувач' },
+        {
+          value: User.USER_ROLE.ADMIN,
+          text: 'Адміністратор',
+        },
+        {
+          value: User.USER_ROLE.DEVELOPER,
+          text: 'Розробник',
+        },
+      ],
+      role2: [
+        { value: 'tfdsf', text: 'Test2' },
+        {
+          value: true,
+          text: 'test3',
+        },
+        {
+          value: 4312,
+          text: 'test',
+        },
+      ],
     },
   })
   // ↑↑ сюди вводимо JSON дані
 })
+
+// router.post('/signup', function (req, res) {
+//   const { email, login, role } = req.body
+
+//   if (!email || !login || !role) {
+//     return res.status(400).json({
+//       message: 'Error',
+//})
+// }
+// })
 
 // Підключаємо роутер до бек-енду
 module.exports = router
